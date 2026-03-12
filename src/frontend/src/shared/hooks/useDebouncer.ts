60ffac56f678df7036a2d68a437cf4e26202c478
vladcheck <valdemarcheck@yandex.ru>
2026-03-12 21:37:14 +0300

    feat: create useDebouncer hook

---
+++ src/frontend/src/shared/hooks/useDebouncer.ts
@@ -0,0 +1,13 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect } from "react";
+
+export default function useDebouncer(
+  cb: () => unknown,
+  ms: number,
+  dependencies: unknown[],
+): void {
+  useEffect(() => {
+    const debouncer = setTimeout(cb, ms);
+    return () => clearTimeout(debouncer);
+  }, [cb, ms, ...dependencies]);
+}
