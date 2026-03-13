d244e1e36b9a925eca39c2084898819aec55319f
vladcheck <valdemarcheck@yandex.ru>
2026-03-13 12:56:35 +0300

    test(db): add new mock user for faster login tests

--- src/server/mock/users.ts
+++ src/server/mock/users.ts
@@ -8,5 +8,12 @@ let users: User[] = [
     lastName: "Овчинников",
     hash: "$2b$10$vTDm6mpcL0IdBOMAn67JcOyyVwUfZgN8iR0yAFgw/eWBNs1/npmPm",
   },
+  {
+    id: "TUu_OE",
+    firstName: "Владимир",
+    lastName: "Валекжанин",
+    email: "valdemarcheck@yandex.ru",
+    hash: "$2b$10$O51Zoad3gxCWDVyR/Y8dBur84v2hC4cwSP5Pr8TYMF/XRd/apCqp2",
+  },
 ];
 export default users;
